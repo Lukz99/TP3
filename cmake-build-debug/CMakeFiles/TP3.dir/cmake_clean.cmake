a97393ee4f58ea3d78891cc619c227f959000b73
Juampi <juampiamboage18@gmail.com>
2021-12-10 16:05:45 -0300

    -Reestructuracion lectura de archivos -Constructores vacíos en clases hijas de edificios -Atributo matriz de costes añadida en clase jugador

---
+++ cmake-build-debug/CMakeFiles/TP3.dir/cmake_clean.cmake
@@ -0,0 +1,34 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TP3.dir/Aserradero.cpp.obj"
+  "CMakeFiles/TP3.dir/Casillero.cpp.obj"
+  "CMakeFiles/TP3.dir/CasilleroConstruible.cpp.obj"
+  "CMakeFiles/TP3.dir/CasilleroInaccesible.cpp.obj"
+  "CMakeFiles/TP3.dir/CasilleroTransitable.cpp.obj"
+  "CMakeFiles/TP3.dir/DatosLeidos.cpp.obj"
+  "CMakeFiles/TP3.dir/DiccionarioDeEdificios.cpp.obj"
+  "CMakeFiles/TP3.dir/Edificio.cpp.obj"
+  "CMakeFiles/TP3.dir/Escuela.cpp.obj"
+  "CMakeFiles/TP3.dir/Fabrica.cpp.obj"
+  "CMakeFiles/TP3.dir/Grafo.cpp.obj"
+  "CMakeFiles/TP3.dir/Jugador.cpp.obj"
+  "CMakeFiles/TP3.dir/ListaMateriales.cpp.obj"
+  "CMakeFiles/TP3.dir/Material.cpp.obj"
+  "CMakeFiles/TP3.dir/Menu.cpp.obj"
+  "CMakeFiles/TP3.dir/Mina.cpp.obj"
+  "CMakeFiles/TP3.dir/MinaOro.cpp.obj"
+  "CMakeFiles/TP3.dir/NodoArbol.cpp.obj"
+  "CMakeFiles/TP3.dir/NodoLista.cpp.obj"
+  "CMakeFiles/TP3.dir/Obelisco.cpp.obj"
+  "CMakeFiles/TP3.dir/PlantaElectrica.cpp.obj"
+  "CMakeFiles/TP3.dir/Vertice.cpp.obj"
+  "CMakeFiles/TP3.dir/main.cpp.obj"
+  "TP3.exe"
+  "TP3.exe.manifest"
+  "TP3.pdb"
+  "libTP3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TP3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
