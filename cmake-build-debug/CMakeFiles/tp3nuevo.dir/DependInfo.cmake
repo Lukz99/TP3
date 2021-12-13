72f6ffe12bc51b6f22fa1f8c0c585520eab52577
Nicolas Gorzio <gorzionicolas@gmail.com>
2021-12-12 23:30:05 -0300

    lluvia de recursos juampi

---
+++ cmake-build-debug/CMakeFiles/tp3nuevo.dir/DependInfo.cmake
@@ -0,0 +1,52 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/ekizu/Desktop/tp3nuevo/Aserradero.cpp" "C:/Users/ekizu/Desktop/tp3nuevo/cmake-build-debug/CMakeFiles/tp3nuevo.dir/Aserradero.cpp.obj"
+  "C:/Users/ekizu/Desktop/tp3nuevo/Casillero.cpp" "C:/Users/ekizu/Desktop/tp3nuevo/cmake-build-debug/CMakeFiles/tp3nuevo.dir/Casillero.cpp.obj"
+  "C:/Users/ekizu/Desktop/tp3nuevo/CasilleroConstruible.cpp" "C:/Users/ekizu/Desktop/tp3nuevo/cmake-build-debug/CMakeFiles/tp3nuevo.dir/CasilleroConstruible.cpp.obj"
+  "C:/Users/ekizu/Desktop/tp3nuevo/CasilleroInaccesible.cpp" "C:/Users/ekizu/Desktop/tp3nuevo/cmake-build-debug/CMakeFiles/tp3nuevo.dir/CasilleroInaccesible.cpp.obj"
+  "C:/Users/ekizu/Desktop/tp3nuevo/CasilleroTransitable.cpp" "C:/Users/ekizu/Desktop/tp3nuevo/cmake-build-debug/CMakeFiles/tp3nuevo.dir/CasilleroTransitable.cpp.obj"
+  "C:/Users/ekizu/Desktop/tp3nuevo/DatosLeidos.cpp" "C:/Users/ekizu/Desktop/tp3nuevo/cmake-build-debug/CMakeFiles/tp3nuevo.dir/DatosLeidos.cpp.obj"
+  "C:/Users/ekizu/Desktop/tp3nuevo/DiccionarioDeEdificios.cpp" "C:/Users/ekizu/Desktop/tp3nuevo/cmake-build-debug/CMakeFiles/tp3nuevo.dir/DiccionarioDeEdificios.cpp.obj"
+  "C:/Users/ekizu/Desktop/tp3nuevo/Edificio.cpp" "C:/Users/ekizu/Desktop/tp3nuevo/cmake-build-debug/CMakeFiles/tp3nuevo.dir/Edificio.cpp.obj"
+  "C:/Users/ekizu/Desktop/tp3nuevo/Escuela.cpp" "C:/Users/ekizu/Desktop/tp3nuevo/cmake-build-debug/CMakeFiles/tp3nuevo.dir/Escuela.cpp.obj"
+  "C:/Users/ekizu/Desktop/tp3nuevo/Fabrica.cpp" "C:/Users/ekizu/Desktop/tp3nuevo/cmake-build-debug/CMakeFiles/tp3nuevo.dir/Fabrica.cpp.obj"
+  "C:/Users/ekizu/Desktop/tp3nuevo/Grafo.cpp" "C:/Users/ekizu/Desktop/tp3nuevo/cmake-build-debug/CMakeFiles/tp3nuevo.dir/Grafo.cpp.obj"
+  "C:/Users/ekizu/Desktop/tp3nuevo/Jugador.cpp" "C:/Users/ekizu/Desktop/tp3nuevo/cmake-build-debug/CMakeFiles/tp3nuevo.dir/Jugador.cpp.obj"
+  "C:/Users/ekizu/Desktop/tp3nuevo/ListaMateriales.cpp" "C:/Users/ekizu/Desktop/tp3nuevo/cmake-build-debug/CMakeFiles/tp3nuevo.dir/ListaMateriales.cpp.obj"
+  "C:/Users/ekizu/Desktop/tp3nuevo/Material.cpp" "C:/Users/ekizu/Desktop/tp3nuevo/cmake-build-debug/CMakeFiles/tp3nuevo.dir/Material.cpp.obj"
+  "C:/Users/ekizu/Desktop/tp3nuevo/Menu.cpp" "C:/Users/ekizu/Desktop/tp3nuevo/cmake-build-debug/CMakeFiles/tp3nuevo.dir/Menu.cpp.obj"
+  "C:/Users/ekizu/Desktop/tp3nuevo/Mina.cpp" "C:/Users/ekizu/Desktop/tp3nuevo/cmake-build-debug/CMakeFiles/tp3nuevo.dir/Mina.cpp.obj"
+  "C:/Users/ekizu/Desktop/tp3nuevo/MinaOro.cpp" "C:/Users/ekizu/Desktop/tp3nuevo/cmake-build-debug/CMakeFiles/tp3nuevo.dir/MinaOro.cpp.obj"
+  "C:/Users/ekizu/Desktop/tp3nuevo/NodoArbol.cpp" "C:/Users/ekizu/Desktop/tp3nuevo/cmake-build-debug/CMakeFiles/tp3nuevo.dir/NodoArbol.cpp.obj"
+  "C:/Users/ekizu/Desktop/tp3nuevo/NodoLista.cpp" "C:/Users/ekizu/Desktop/tp3nuevo/cmake-build-debug/CMakeFiles/tp3nuevo.dir/NodoLista.cpp.obj"
+  "C:/Users/ekizu/Desktop/tp3nuevo/Obelisco.cpp" "C:/Users/ekizu/Desktop/tp3nuevo/cmake-build-debug/CMakeFiles/tp3nuevo.dir/Obelisco.cpp.obj"
+  "C:/Users/ekizu/Desktop/tp3nuevo/Objetivos.cpp" "C:/Users/ekizu/Desktop/tp3nuevo/cmake-build-debug/CMakeFiles/tp3nuevo.dir/Objetivos.cpp.obj"
+  "C:/Users/ekizu/Desktop/tp3nuevo/PlantaElectrica.cpp" "C:/Users/ekizu/Desktop/tp3nuevo/cmake-build-debug/CMakeFiles/tp3nuevo.dir/PlantaElectrica.cpp.obj"
+  "C:/Users/ekizu/Desktop/tp3nuevo/Vertice.cpp" "C:/Users/ekizu/Desktop/tp3nuevo/cmake-build-debug/CMakeFiles/tp3nuevo.dir/Vertice.cpp.obj"
+  "C:/Users/ekizu/Desktop/tp3nuevo/main.cpp" "C:/Users/ekizu/Desktop/tp3nuevo/cmake-build-debug/CMakeFiles/tp3nuevo.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
